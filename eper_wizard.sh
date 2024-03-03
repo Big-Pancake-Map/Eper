454e6c8aa6e401abbb359a2da0c5e784b17a57d7
Szabolcs Tóth <tsz@purzelbaum.hu>
2024-03-03 14:58:59 +0100

    New: User input testing..

--- eper_wizard.sh
+++ eper_wizard.sh
@@ -4,52 +4,52 @@ echo "***************************"
 echo "* Welcome to 🍓 Wizard 🪄  *"
 echo "***************************"
 
-echo -e "\n1️⃣  - Get updates."
-
-sudo apt-get update
-
-echo -e "\n2️⃣  - Upgrade the system."
-sudo apt-get upgrade -y
-
-echo -e "\n3️⃣  - Install a few prerequisite packages which let apt use packages over HTTPS."
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-
-echo -e "\n4️⃣  - Then add the GPG key for the official Docker repository to your system."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo -e "\n5️⃣  - Add the Docker repository to APT sources."
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-echo -e "\n6️⃣  - Update your existing list of packages again for the addition to be recognized."
-sudo apt update
-
-echo -e "\n7️⃣  - Make sure you are about to install from the Docker repo instead of the default Ubuntu repo."
-apt-cache policy docker-ce
-
-echo -e "\n8️⃣  - Intall Docker."
-sudo apt install docker-ce
-
-echo -e "\n9️⃣  - Install Docker Compose."
-sudo apt-get install docker-compose-plugin
-
-echo -e "\n🔟 - Check Docker Compose version."
-docker compose version
-
-echo -e "\n1️⃣ 1️⃣  - Install NGINX."
-sudo apt install nginx
-
-echo -e "\n1️⃣ 2️⃣  - start NGINX."
-sudo systemctl start nginx.service
-
-echo -e "\n1️⃣ 3️⃣  - Prepare port 80."
-sudo iptables -I INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-sudo iptables -I OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-
-echo -e "\n1️⃣ 4️⃣  - Remove the default configration file."
-FILE=/etc/nginx/sites-enabled/default
-if [ -f "$FILE" ]; then
-	sudo unlink "$FILE"
-fi
+#echo -e "\n1️⃣  - Get updates."
+#
+#sudo apt-get update
+#
+#echo -e "\n2️⃣  - Upgrade the system."
+#sudo apt-get upgrade -y
+#
+#echo -e "\n3️⃣  - Install a few prerequisite packages which let apt use packages over HTTPS."
+#sudo apt install apt-transport-https ca-certificates curl software-properties-common
+#
+#echo -e "\n4️⃣  - Then add the GPG key for the official Docker repository to your system."
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#
+#echo -e "\n5️⃣  - Add the Docker repository to APT sources."
+#echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#
+#echo -e "\n6️⃣  - Update your existing list of packages again for the addition to be recognized."
+#sudo apt update
+#
+#echo -e "\n7️⃣  - Make sure you are about to install from the Docker repo instead of the default Ubuntu repo."
+#apt-cache policy docker-ce
+#
+#echo -e "\n8️⃣  - Intall Docker."
+#sudo apt install docker-ce
+#
+#echo -e "\n9️⃣  - Install Docker Compose."
+#sudo apt-get install docker-compose-plugin
+#
+#echo -e "\n🔟 - Check Docker Compose version."
+#docker compose version
+#
+#echo -e "\n1️⃣ 1️⃣  - Install NGINX."
+#sudo apt install nginx
+#
+#echo -e "\n1️⃣ 2️⃣  - start NGINX."
+#sudo systemctl start nginx.service
+#
+#echo -e "\n1️⃣ 3️⃣  - Prepare port 80."
+#sudo iptables -I INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+#sudo iptables -I OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+#
+#echo -e "\n1️⃣ 4️⃣  - Remove the default configration file."
+#FILE=/etc/nginx/sites-enabled/default
+#if [ -f "$FILE" ]; then
+#	sudo unlink "$FILE"
+#fi
 
 echo -e "\n1️⃣ 5️⃣  - Create new nginx reverse proxy configuration file."
 read -p "What is your app name: " app_name
